61cd1b4a66b746a67056524d3c6c46b0b089fc98
kdogz9 <2401317@students.ucreative.ac.uk>
2024-10-25 15:32:44 +0100

    Added Project

---
+++ Platformer/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class RotatingObject : MonoBehaviour
+{
+    public Vector3 rotationDirection = Vector3.up;
+    public float rotationSpeed = 50f;
+
+    private void Update()
+    {
+        transform.Rotate(rotationDirection.normalized, rotationSpeed * Time.deltaTime);
+    }
+}
