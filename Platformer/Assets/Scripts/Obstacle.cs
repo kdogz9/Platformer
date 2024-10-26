599426ff6ec45a6ddaa026a7ad2721c79b0ced29
kdogz9 <2401317@students.ucreative.ac.uk>
2024-10-26 16:24:05 +0100

    today

--- Platformer/Assets/Scripts/Obstacle.cs
+++ Platformer/Assets/Scripts/Obstacle.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-
-
 public class Obstacle : MonoBehaviour
 {
+    public TMP_Text DeathText;
 
- }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))  // Ensure that only the player triggers this action
+        {
+            Destroy(other.gameObject);   // Destroy the entire GameObject
+            DeathText.text = "You died";
+            Debug.Log("You died");
+        }
+    }
+}
