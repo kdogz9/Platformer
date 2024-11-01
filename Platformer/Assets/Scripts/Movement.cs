9e2c72b4275091f47e8275268c1575f430c14524
kdogz9 <2401317@students.ucreative.ac.uk>
2024-11-01 16:30:59 +0000

    code

--- Platformer/Assets/Scripts/Movement.cs
+++ Platformer/Assets/Scripts/Movement.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class MoveBetweenPoints : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // References to the two target points
+    [SerializeField] private Transform point0;
+    [SerializeField] private Transform point1;
 
-    // Update is called once per frame
-    void Update()
+    // Speed of movement
+    [SerializeField] private float speed = 2.0f;
+
+    // Direction flag
+    private bool isForward = true;
+
+    private void Update()
     {
-        
+        // Move towards point1 if moving forward, otherwise move towards point0
+        if (isForward)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, point1.position, speed * Time.deltaTime);
+
+            // Check if the object has reached point1
+            if (transform.position == point1.position)
+            {
+                isForward = false; // Change direction
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, point0.position, speed * Time.deltaTime);
+
+            // Check if the object has reached point0
+            if (transform.position == point0.position)
+            {
+                isForward = true; // Change direction
+            }
+        }
     }
 }
+
+ 
